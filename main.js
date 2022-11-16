b3b7e13b2a37577a0f85e0808568795374b9ad7d
KMinetto <killian.minetto@gmail.com>
2022-11-16 18:12:44 +0100

    Hide menu

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ const { app, BrowserWindow } = require("electron");
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 400,
-        height: 680
+        height: 650
     });
 
+    win.setMenu(null);
     win.loadFile("index.html");
 }
 
